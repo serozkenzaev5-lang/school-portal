4cf8860edb1ef98f8598a37a8cba65bcc3380ce5
serozkenzaev5-lang <serozkenzaev5@gmail.com>
2026-08-13 20:36:35 +0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,21 @@
-// Ваши ключи из консоли Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Конфигурация твоего Firebase проекта
 const firebaseConfig = {
-  apiKey: "AIzaSyDQcXc5Pvl3MedAuOlpH2Dvr2VTlXwc-jM",
+  // Твои ключи Firebase (вставь свои, если отличаются)
+  apiKey: "AIzaSy...", 
   authDomain: "school-portal-ff938.firebaseapp.com",
   projectId: "school-portal-ff938",
-  storageBucket: "school-portal-ff938.firebasestorage.app",
-  messagingSenderId: "569193226135",
-  appId: "1:569193226135:web:8f7b901a8f288d37e1eed2",
-  measurementId: "G-NVQG0DTXB3"
+  storageBucket: "school-portal-ff938.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
-// Инициализация Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// 1. Инициализация приложения
+const app = initializeApp(firebaseConfig);
 
-// Делаем базы данных доступными для dashboard.js
-window.db = firebase.firestore();
-window.auth = firebase.auth();
+// 2. Инициализация и ОБЯЗАТЕЛЬНЫЙ ЭКСПОРТ auth и db
+export const auth = getAuth(app);
+export const db = getFirestore(app);
